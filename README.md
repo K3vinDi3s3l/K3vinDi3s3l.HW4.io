4fe5bdd228d7a8a315d07b96b704c4b9d256f047
Kevin Diesel <kaos@Kevins-MacBook-Pro.local>
2020-03-21 13:55:21 -0400

    html, js , readme files uploaded

--- README.md
+++ README.md
@@ -1,63 +1,23 @@
-# Project Title
+# Title 
 
-One Paragraph of project description goes here
+JavasScript Code Quiz!
 
-## Getting Started
+## Project Descritpion
 
-These instructions will get you a copy of the project up and running on your local machine for development and testing purposes. See deployment for notes on how to deploy the project on a live system.
+This a quiz that will allow you to test your JavaSript knowledge by reviewing and
+answering a few questions. After every question you answer you will be provided with a Correct/Wrong result as well as a grade in the end of the quiz.
 
-### Prerequisites
+### Screenshots
+![](img.1.png)
+![](img.2.png)
+![](img.3.png)
+![](img.4.png)
 
-What things you need to install the software and how to install them
+### How to access the quiz
 
-```
-Give examples
-```
+Please follow this URL and you will be directed to the quiz.
 
-### Installing
+### Built With
 
-A step by step series of examples that tell you how to get a development env running
-
-Say what the step will be
-
-```
-Give the example
-```
-
-And repeat
-
-```
-until finished
-```
-
-End with an example of getting some data out of the system or using it for a little demo
-
-## Running the tests
-
-Explain how to run the automated tests for this system
-
-### Break down into end to end tests
-
-Explain what these tests test and why
-
-```
-Give an example
-```
-
-### And coding style tests
-
-Explain what these tests test and why
-
-```
-Give an example
-```
-
-## Deployment
-
-Add additional notes about how to deploy this on a live system
-
-## Built With
-
-* [Dropwizard](http://www.dropwizard.io/1.0.2/docs/) - The web framework used
-* [Maven](https://maven.apache.org/) - Dependency Management
-* [ROME](https://rometools.github.io/rome/) - Used to generate RSS Feeds
+* HTLM (https://developer.mozilla.org/en-US/docs/Glossary/HTML#Concept_and_syntax)
+* Jacascript (https://developer.mozilla.org/en-US/docs/Web/JavaScript)
---
+++ codequiz.js
@@ -0,0 +1,193 @@
+
+// Qustions / Answers that will pop up when you click the button. // 
+const quizQuestions = [
+    {
+        question: "What do CSS stand for?",
+        answers: {
+            A: "Cascading Style Sheet",
+            B: "Create Style Sheet",
+            C: "Common Style Sheet",
+            D: "Code System Sheet"
+        },
+        correctAnswer: "A",
+    },
+    {
+        question: "Who maintains Javascript?",
+        answers: {
+            A: "World Wide Web Consortium",
+            B: "Google",
+            C: "Microsoft",
+            D: "Apple"
+        },
+        correctAnswer: "A",
+    },
+    {
+        question: "What is the correct way to make a function in javascipt?",
+        answers: {
+            A: "function() name{}",
+            B: "function name(){}",
+            C: "function name{}()",
+            D: "name() function{}"
+        },
+        correctAnswer: "B",
+    },
+    {
+        question: "What does NaN stand for?",
+        answers: {
+            A: "Negative answer Notation",
+            B: "New asyncronous Number",
+            C: "Not a Number",
+            D: "Next amount Now"
+        },
+        correctAnswer: "C",
+    },
+];
+const timeAmount = 80000;
+const quizQuestionContainer = document.getElementById("question");
+const quizAnswersContainer = document.getElementById("answers");
+const answerResultContainer = document.getElementById("result");
+const nextSetButton = document.getElementById("next");
+const timerElem = document.getElementById("timer");
+
+
+var questionSetCounter = 0;
+var answerCounter = 0;
+var answerElementCount = 0;
+
+var highscores = [];
+var score = 0;
+var time = 80;
+
+function startTimer() {
+
+    var counter = setInterval(timer, 1000);
+
+    function timer() {
+        time = time - 1;
+        if (time <= 0) {
+            clearInterval(counter);
+            // counter ended, do something here //
+            return;
+        }
+
+        timerElem.innerHTML = time;
+
+    }
+}
+
+function chosenAnswer() {
+    var answer = document.getElementById(event.srcElement.id);
+
+
+    answerArray = answer.innerHTML.split('');
+
+    if (answerArray[0] === quizQuestions[questionSetCounter - 1].correctAnswer) {
+        answerResultContainer.innerHTML = "Correct!";
+        score += 5;
+    } else {
+        answerResultContainer.innerHTML = "Wrong!";
+    }
+    getNextQuestionSet();
+}
+
+
+function answerButton(letter, answer) {
+    var btn = document.createElement("BUTTON");
+    btn.type = "button";
+    btn.id = `chosen${answerCounter}`;
+    btn.onclick = chosenAnswer;
+    btn.class = "btn btn-primary";
+    btn.innerHTML = `${letter}: ${answer}`;
+
+    answerCounter++;
+    return btn;
+}
+
+
+function getNextQuestionSet() {
+    var answerSet = [];
+
+
+    while (quizAnswersContainer.firstChild) {
+        quizAnswersContainer.removeChild(quizAnswersContainer.firstChild);
+    }
+
+    if (time !== 0 && questionSetCounter < quizQuestions.length) {
+        if (nextSetButton) {
+            nextSetButton.remove();
+        }
+
+
+        for (answer in quizQuestions[questionSetCounter].answers) {
+            answerSet.push(
+                answerButton(answer, quizQuestions[questionSetCounter].answers[answer])
+            )
+        }
+
+        quizQuestionContainer.innerHTML = quizQuestions[questionSetCounter].question;
+
+        for (var i = 0; i < answerSet.length; i++) {
+            quizAnswersContainer.appendChild(answerSet[i]);
+        }
+
+        questionSetCounter++;
+    } else {
+        showResults();
+    }
+
+}
+
+function showResults() {
+    var inputElem = document.createElement("INPUT");
+    inputElem.setAttribute("type", "text");
+    inputElem.id = "initials";
+
+    var submitBtn = document.createElement("BUTTON");
+    submitBtn.type = "button";
+    submitBtn.id = "submit";
+    submitBtn.onclick = submitScore;
+    submitBtn.class = "btn btn-primary";
+    submitBtn.innerHTML = "Submit";
+
+    answerResultContainer.innerHTML = '';
+    quizQuestionContainer.innerHTML = `Your score is ${score}!`;
+    quizAnswersContainer.innerHTML = "Initials: ";
+    quizAnswersContainer.appendChild(inputElem);
+    quizAnswersContainer.appendChild(submitBtn);
+}
+
+function submitScore() {
+    var userInitial = document.getElementById("initials").value;
+    highscores.push(
+        {
+            Initials: userInitial,
+            Score: score,
+        }
+    )
+
+    highscoresPage();
+}
+
+function highscoresPage() {
+
+    while (quizAnswersContainer.firstChild) {
+        quizAnswersContainer.removeChild(quizAnswersContainer.firstChild);
+    }
+
+    var title = document.getElementById("title");
+    var listScores = "";
+    title.innerHTML = "Highscores";
+
+    for (var i = 0; i < highscores.length; i++) {
+
+        listScores = listScores + `${highscores[i].Initials}: ${highscores[i].Score} </br>`;
+    }
+
+    quizQuestionContainer.innerHTML = listScores;
+
+}
+
+nextSetButton.addEventListener('click', () => {
+    startTimer();
+    getNextQuestionSet();
+});
